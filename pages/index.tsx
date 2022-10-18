1d6754a8c0e8f5c9667a68e75e54241db2ab835c
RolandoPranataAR <rolandopranata@gmail.com>
2022-10-18 15:43:58 +0700

    chore: convert landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
+import AOS from 'aos';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
       <section>
         <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href="/#">
               <svg
                 width="60"
                 height="60"
@@ -131,15 +136,11 @@ const Home: NextPage = () => {
             <div className="col-lg-6 col-12 d-lg-block d-none">
               <div className="d-flex justify-content-lg-end justify-content-center me-lg-5">
                 <div className="position-relative" data-aos="zoom-in">
-                  <img
-                    src="./assets/img/Header-1.png"
-                    className="img-fluid"
-                    alt=""
-                  />
+                  <img src="/img/Header-1.png" className="img-fluid" alt="" />
                   <div className="card left-card position-absolute border-0">
                     <div className="d-flex align-items-center mb-16 gap-3">
                       <img
-                        src="./assets/img/Header-2.png"
+                        src="/img/Header-2.png"
                         width="40"
                         height="40"
                         className="rounded-pill"
@@ -220,7 +221,7 @@ const Home: NextPage = () => {
                   <div className="card right-card position-absolute border-0">
                     <div className="position-relative d-flex flex-row justify-content-center mb-24">
                       <img
-                        src="./assets/img/Header-3.png"
+                        src="/img/Header-3.png"
                         className="rounded-pill"
                         alt=""
                       />
@@ -509,7 +510,7 @@ const Home: NextPage = () => {
               <a href="./src/detail.html">
                 <div className="blur-sharp">
                   <img
-                    src="./assets/img/Thumbnail-1.png"
+                    src="/img/Thumbnail-1.png"
                     width="205"
                     height="270"
                     alt=""
@@ -545,7 +546,7 @@ const Home: NextPage = () => {
               <a href="./src/detail.html">
                 <div className="blur-sharp">
                   <img
-                    src="./assets/img/Thumbnail-2.png"
+                    src="/img/Thumbnail-2.png"
                     width="205"
                     height="270"
                     alt=""
@@ -581,7 +582,7 @@ const Home: NextPage = () => {
               <a href="./src/detail.html">
                 <div className="blur-sharp">
                   <img
-                    src="./assets/img/Thumbnail-3.png"
+                    src="/img/Thumbnail-3.png"
                     width="205"
                     height="270"
                     alt=""
@@ -617,7 +618,7 @@ const Home: NextPage = () => {
               <a href="./src/detail.html">
                 <div className="blur-sharp">
                   <img
-                    src="./assets/img/Thumbnail-4.png"
+                    src="/img/Thumbnail-4.png"
                     width="205"
                     height="270"
                     alt=""
@@ -653,7 +654,7 @@ const Home: NextPage = () => {
               <a href="./src/detail.html">
                 <div className="blur-sharp">
                   <img
-                    src="./assets/img/Thumbnail-5.png"
+                    src="/img/Thumbnail-5.png"
                     width="205"
                     height="270"
                     alt=""
@@ -742,7 +743,7 @@ const Home: NextPage = () => {
               data-aos="zoom-in"
             >
               <img
-                src="./assets/img/Header-9.png"
+                src="/img/Header-9.png"
                 width="612"
                 height="452"
                 className="img-fluid"

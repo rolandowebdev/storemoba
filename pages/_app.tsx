a3184d81139d13398c6b237f2618135101b861b8
RolandoPranataAR <rolandopranata@gmail.com>
2022-10-19 23:50:34 +0700

    chore: convert checkout page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/utilities.css';
 import '../styles/homepage.css';
 import '../styles/detail.css';
+import '../styles/checkout.css';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 

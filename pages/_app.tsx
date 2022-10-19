44d9b5d535e26ef44019aa88deacaf6013d70e3d
RolandoPranataAR <rolandopranata@gmail.com>
2022-10-20 01:12:04 +0700

    chore: convert edit profile page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import '../styles/sidebar.css';
 import '../styles/overview.css';
 import '../styles/transactions.css';
 import '../styles/transactions-detail.css';
+import '../styles/edit-profile.css';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 

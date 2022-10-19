2f0d8af8e71681a5eb59a0478f3ae9f3acb3d242
RolandoPranataAR <rolandopranata@gmail.com>
2022-10-20 00:55:47 +0700

    chore: convert transactions page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import '../styles/sign-up-success.css';
 import '../styles/404.css';
 import '../styles/sidebar.css';
 import '../styles/overview.css';
+import '../styles/transactions.css';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
